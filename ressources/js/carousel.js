de9a20d6f464e64a6ad63efe07b68edd03bb79ba
frankup1 <francis.bedard.4@ulaval.ca>
2015-09-20 19:52:01 -0400

    Page principale. Il reste 'a mettre la liste de films dans le bas de la page.(voir image Facebook conversation 2015-09-18).

--- ressources/js/carousel.js
+++ ressources/js/carousel.js
@@ -4,3 +4,10 @@ function galleryspin(sign) {
     if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
     spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
 }
+setInterval(interval, 4000 );
+
+function interval()
+{
+    galleryspin('');
+}
+
