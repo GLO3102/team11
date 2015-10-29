090631dd7db22af49e1c02376a3e9954e5f8af96
frankup1 <francis.bedard.4@ulaval.ca>
2015-10-29 19:29:54 -0400

    Requirejs
    Avec view ajouter pour toute les partie du menu

--- ressources/js/main.js
+++ ressources/js/main.js
@@ -1,8 +1,14 @@
 require.config({
     paths: {
+        jquery:'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery',
         underscore: 'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore',
         backbone: 'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone',
-        text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text'
+        text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text',
+        bootstrap:'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap'
+    },
+    shim:{
+        'bootstrap' :{
+            deps:['jquery']}
     }
 
 });
