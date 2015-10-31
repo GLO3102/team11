fd5ab170a3b6c2ead6859e5ee40af92d0e37e685
frankup1 <francis.bedard.4@ulaval.ca>
2015-10-31 12:03:50 -0400

    2e version recherche global.

--- ressources/js/view/general_search.js
+++ ressources/js/view/general_search.js
@@ -8,7 +8,7 @@ define([
 
 var General_SearchView = Backbone.View.extend({
     template: _.template(GSTemplate),
-    el: $("#main_container"),
+    el: $("#browse"),
     render: function(){
         this.$el.html(this.template({results:this.collection.toJSON()}));
     }
