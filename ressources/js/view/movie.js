6b5f84caf38d498bb0866499ecae5d790944f3db
timotheepicard <ptim64@gmail.com>
2015-11-02 16:54:43 -0500

    add movie view and template

--- ressources/js/view/movie.js
+++ ressources/js/view/movie.js
@@ -1,3 +1,32 @@
 /**
  * Created by Timothée on 29/10/2015.
  */
+define([
+    'backbone',
+    'utils/utils',
+    'text!template/movie_template.html'
+], function(Backbone,MovieTemplate) {
+
+    var MovieView = Backbone.View.extend({
+        template: _.template(MovieTemplate),
+
+        el: '#main_container',
+
+        initialize: function () {
+            _.bindAll(this, 'render');
+
+            var that = this;
+
+            this.model.bind('sync', function () {
+                that.render();
+            });
+        },
+
+        render: function () {
+            this.$el.html(MovieTemplate);
+            return this;
+        }
+    });
+
+    return MovieView;
+});
