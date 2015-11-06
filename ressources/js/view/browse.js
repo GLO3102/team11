4ab08df7aaa3b21cc7c0b248be9e318a6f001c37
Manuel Pavone <pavone.pro@gmail.com>
2015-11-06 09:47:40 -0500

    Correction bouton home quand on accède à une film / série

--- ressources/js/view/browse.js
+++ ressources/js/view/browse.js
@@ -11,17 +11,18 @@ define([
 
     var BrowseView = Backbone.View.extend({
         template: _.template(BrowseTemplate),
-        el: $("#browse"),
+        el: "#main_container",
+
         initialize:function(){
             _.bindAll(this,'render');
         },
         render: function(){
             var self = this;
             var gSearch = BrowseCollection.extend({url: 'http://localhost:3000/unsecure/'});
-            gSearch.search('all&limit=20').done(function(results){
-                self.$el.html(self.template({resultsCol:results.toJSON()}));
-            });
 
+            gSearch.search('all&limit=20').done(function(results){
+                self.$el.append(self.template({resultsCol:results.toJSON()}));
+            });
             var moviesGenreCol = new CollectionGenre();
             moviesGenreCol.url ='http://localhost:3000/unsecure/genres/movies';
             var genremovie = new GenreView({el:'#listMovies',collection:moviesGenreCol});
