83ebf6154c18dd683a70ce213d0e55990b0a7a7b
frankup1 <francis.bedard.4@ulaval.ca>
2015-12-10 21:27:39 -0500

    Filtre de recherche débuter fonctionnel, mais il reste un bogues à réglé, car pafois quand il y a 2 sélection ça fonctionne dautre fois non.

--- ressources/js/view/general_search.js
+++ ressources/js/view/general_search.js
@@ -14,12 +14,15 @@ var General_SearchView = Backbone.View.extend({
     template: _.template(GSTemplate),
     el: "#browseContainer",
     events:{
-        'click #addWatchlist' : 'addWatchlist'
+        'click #addWatchlist': 'addWatchlist',
+        'click #allFilter': 'allGenre',
+        'click .genreFilter': 'filterByGenre'
     },
     render: function(){
         var that = this;
             var resultJSON = this.collection.toJSON();
             var displayCol = [];
+        var genreFilter = [];
             var title = '';
             var by = '';
             var shortDesc = '';
@@ -27,12 +30,21 @@ var General_SearchView = Backbone.View.extend({
             var artworkUrl100;
             var type ='';
             var id='';
+        var pushGenreIfNotExist = function (val) {
+            if (typeof(val) == 'undefined' || val == '') {
+                return
+            }
+            val = $.trim(val)
+            if ($.inArray(val, genreFilter) == -1) {
+                genreFilter.push(val);
+            }
+        };
         if (typeof (resultJSON[0].results) != "undefined") {
             for (var i = 0; i < resultJSON[0].results.length; i++) {
                 var data = resultJSON[0].results[i];
                 id = data.trackId;
                 type = data.wrapperType;
-
+                pushGenreIfNotExist(data.primaryGenreName);
                 if (data.wrapperType == "track") {
                     title = data.trackName
                     by = data.artistName
@@ -47,6 +59,7 @@ var General_SearchView = Backbone.View.extend({
                     url = '#/actors/' + data.artistId;
                 }
                 else {
+                    pushGenreIfNotExist(data.primaryGenreName);
                     title = data.artistName
                     by = data.collectionName
                     shortDesc = data.longDescription;
@@ -101,7 +114,12 @@ var General_SearchView = Backbone.View.extend({
         watchlistCollection.fetch({
             success: function (watchlists) {
 
-                that.$el.html(that.template({results:displayCol, watchlist:watchlists.toJSON(), idUser: idCurrentUser}));
+                that.$el.html(that.template({
+                    results: displayCol,
+                    watchlist: watchlists.toJSON(),
+                    idUser: idCurrentUser,
+                    genres: genreFilter
+                }));
             }
         });
 
@@ -133,6 +151,105 @@ var General_SearchView = Backbone.View.extend({
                 })
             }
         });
+    },
+    allGenre: function () {
+        if ($('#allFilter').is(':checked')) {
+            $('.genreFilter').prop('checked', false);
+            this.render();
+        }
+    },
+    filterByGenre: function () {
+        if ($('.genreFilter').is(':checked')) {
+            $('#allFilter').prop('checked', false);
+            var checkedFilter = $('.genreFilter');
+            var checkedGenre = []
+            for (var i = 0; i < checkedFilter.length; i++) {
+                if (checkedFilter[i].parentElement.innerText === $('.genreFilter')[i].parentElement.innerText) {
+                    if ($('.genreFilter')[i].checked) {
+                        checkedGenre.push($('.genreFilter')[i].parentElement.innerText);
+                    }
+                }
+            }
+
+            var that = this;
+            var resultJSON = this.collection.toJSON();
+            var displayCol = [];
+            var genreFilter = [];
+            var title = '';
+            var by = '';
+            var shortDesc = '';
+            var url = '';
+            var artworkUrl100;
+            var type = '';
+            var id = '';
+            var pushGenreIfNotExist = function (val) {
+                if (typeof(val) == 'undefined' || val == '') {
+                    return
+                }
+                val = $.trim(val)
+                if ($.inArray(val, genreFilter) == -1) {
+                    genreFilter.push(val);
+                }
+            };
+            for (var i = 0; i < resultJSON[0].results.length; i++) {
+                var data = resultJSON[0].results[i];
+                pushGenreIfNotExist(data.primaryGenreName);
+                if ($.inArray(data.primaryGenreName, checkedGenre) != -1) {
+                    id = data.trackId;
+                    type = data.wrapperType;
+                    if (data.wrapperType == "track") {
+                        title = data.trackName
+                        by = data.artistName
+                        shortDesc = data.longDescription;
+                        if (data.longDescription != undefined && data.longDescription.length > 80)
+                            shortDesc = data.longDescription.substring(0, 80);
+                        url = '#/movies/' + data.trackId;
+                    }
+                    else {
+                        title = data.artistName
+                        by = data.collectionName
+                        shortDesc = data.longDescription;
+                        if (data.longDescription != undefined && data.longDescription.length > 80)
+                            shortDesc = data.longDescription.substring(0, 80);
+                        url = '#/tvshows/seasons/' + data.collectionId
+                    }
+                    artworkUrl100 = resultJSON[0].results[i].artworkUrl100;
+                    displayCol.push({
+                        title: title,
+                        by: by,
+                        shortDesc: shortDesc,
+                        url: url,
+                        artworkUrl100: artworkUrl100,
+                        type: type,
+                        trackId: id,
+                    });
+                }
+            }
+            var idCurrentUser = $.cookie('user_id');
+            var watchlistCollection = new WatchListCollection();
+            watchlistCollection.url = URL + '/watchlists';
+            watchlistCollection.fetch({
+                success: function (watchlists) {
+
+                    that.$el.html(that.template({
+                        results: displayCol,
+                        watchlist: watchlists.toJSON(),
+                        idUser: idCurrentUser,
+                        genres: genreFilter
+                    }));
+                    $('#allFilter').prop('checked', false);
+                    for (var i = 0; i < checkedFilter.length; i++) {
+                        if (checkedFilter[i].parentElement.innerText === $('.genreFilter')[i].parentElement.innerText) {
+                            $('.genreFilter')[i].checked = checkedFilter[i].checked;
+                        }
+                    }
+                }
+            });
+        }
+        else {
+            $('#allFilter').prop('checked', true);
+            this.render();
+        }
     }
 
 });
